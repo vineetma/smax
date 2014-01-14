9acb2e223a2dcb6c1a08656cb003ce138ae67b3e
gaurav <gauravyadav32@gmail.com>
2014-01-14 17:06:56 +0530

    updated project properties

--- src/com/dkt/smax/Person.java
+++
@@ -1,5 +0,0 @@
-package com.dkt.smax;
-
-public class Person {
-
-}
--- src/com/dkt/smax/Student.java
+++ src/com/dkt/smax/Student.java
@@ -1,10 +1,10 @@
 package com.dkt.smax;
 
 public class Student {
-String branch; 
-int sem;
-int section;
-int rollno;
-String name;
+protected String branch; 
+protected int sem;
+protected int section;
+protected int rollno;
+protected String name;
 
 }
