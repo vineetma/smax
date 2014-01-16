4be05339b244edc677c7b94f0e90b66af7c4d2b5
gaurav <gauravyadav32@gmail.com>
2014-01-16 16:22:36 +0530

    added get/set

--- src/com/dkt/smax/DaySchedule.java
+++ src/com/dkt/smax/DaySchedule.java
@@ -15,6 +15,13 @@ public class DaySchedule {
 	enumDayName getNameOfDay() {
 		return this.dayName;
 	}
+	void setNameofDay(enumDayName d) {
+		this.dayName=d;
+	}
+	void setTypeOfDay(enumTypeOfDay f ) {
+		this.tod=f;
+		
+	}
 	protected List<TimeSlot> timeSlots;
 	public DaySchedule() {
 		this.tod = DaySchedule.enumTypeOfDay.STUDYDAY;
