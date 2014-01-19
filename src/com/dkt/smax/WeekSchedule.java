20f41ce43e783b40f94fb5f4bdfcf02a96b0ce18
ankurrghv <ankur.raghav745@gmail.com>
2014-01-19 16:05:54 +0530

    added day in weekSchedule and week in time

--- src/com/dkt/smax/WeekSchedule.java
+++ src/com/dkt/smax/WeekSchedule.java
@@ -6,6 +6,25 @@ public class WeekSchedule {
 	protected Time dayStartingTime;
 	protected int duration; // in minutes
 	protected List<DaySchedule> daySchedules;
-	
-	
+	protected int day;
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public WeekSchedule(day d){
+		this.day=d.day;
+	}
+	public WeekSchedule(int day){
+		this.day = day;
+	public WeekSchedule add(day d){
+		WeekSchedule newday= new day(this);
+		newday.day+= d.day;
+		return newday   ;
+		
+		
+	}
 }
+}
+   
