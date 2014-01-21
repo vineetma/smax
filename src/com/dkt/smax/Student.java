fbc666527f3d410271886bb70fcdcb053bd9e45a
gaurav <gauravyadav32@gmail.com>
2014-01-21 11:00:53 +0530

    add

--- src/com/dkt/smax/Student.java
+++ src/com/dkt/smax/Student.java
@@ -1,6 +1,30 @@
 package com.dkt.smax;
 
 public class Student extends Person {
+protected String getBranch() {
+		return branch;
+	}
+	protected void setBranch(String branch) {
+		this.branch = branch;
+	}
+	protected int getSem() {
+		return sem;
+	}
+	protected void setSem(int sem) {
+		this.sem = sem;
+	}
+	protected int getSection() {
+		return section;
+	}
+	protected void setSection(int section) {
+		this.section = section;
+	}
+	protected int getRollno() {
+		return rollno;
+	}
+	protected void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
 protected String branch; 
 protected int sem;
 protected int section;
--- src/com/dkt/smax/Teacher.java
+++ src/com/dkt/smax/Teacher.java
@@ -4,5 +4,11 @@ import java.util.List;
 
 public class Teacher extends Person {
 	List <Subject> subjects;
+	public List<Subject> getSubjects() {
+		return subjects;
+	}
+	public void setSubjects(List<Subject> subjects) {
+		this.subjects = subjects;
+	}
 	public int getPersonType() { return 2; }
 }
