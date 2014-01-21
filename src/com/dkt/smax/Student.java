d715b43ec5a4f06b501f6df053ac57958ba5b7be
Gauravyadav32 <Gaurav@Gaurav-PC>
2014-01-21 13:56:26 +0530

    added

--- src/com/dkt/smax/Student.java
+++ src/com/dkt/smax/Student.java
@@ -1,39 +1,60 @@
 package com.dkt.smax;
 
-public class Student extends Person {
+import java.util.ArrayList;
+import java.util.List;
 
-protected String getBranch() {
+public class Student extends Person {
+	protected String branch;
+	protected int sem;
+	protected int section;
+	protected int rollno;
+
+	protected String getBranch() {
 		return branch;
 	}
+
 	protected void setBranch(String branch) {
 		this.branch = branch;
 	}
+
 	protected int getSem() {
 		return sem;
 	}
+
 	protected void setSem(int sem) {
 		this.sem = sem;
 	}
+
 	protected int getSection() {
 		return section;
 	}
+
 	protected void setSection(int section) {
 		this.section = section;
 	}
+
 	protected int getRollno() {
 		return rollno;
 	}
+
 	protected void setRollno(int rollno) {
 		this.rollno = rollno;
 	}
-protected String branch; 
-protected int sem;
-protected int section;
-protected int rollno;	
 
+	public int getPersonType() {
+		return 1;
+	}
 
-		
-	public int getPersonType() { return 1; }
+	public TimeTable getTimeTable() {
+		TimeTable t1 = new TimeTable();
+		return t1;
+	}
+	public List<Student> getStudentlist(){
+List<Student> t2 = new ArrayList<Student>();
+return t2;
+
+	}
+
 	public Student(String name, int rollno, int sec, int section, String branch) {
 		super(name);
 		this.rollno = rollno;
@@ -41,6 +62,3 @@ protected int rollno;
 		this.branch = branch;
 	}
 }
-
-	
-
---
+++ src/com/dkt/smax/tests/StudentTest.java
@@ -0,0 +1,59 @@
+package com.dkt.smax.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTest {
+
+	@Test
+	public void testGetPersonType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBranch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBranch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRollno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRollno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStudent() {
+		fail("Not yet implemented");
+	}
+
+}
